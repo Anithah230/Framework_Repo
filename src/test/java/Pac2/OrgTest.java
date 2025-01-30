95554fcf868083c13238657c90d893864e33fb87
anith <anith@ANITHA.mshome.net>
2025-01-30 17:05:39 +0530

    First Push

---
+++ src/test/java/Pac2/OrgTest.java
@@ -0,0 +1,17 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void CreateOrgTest() {
+		System.out.println("excute CreateOrgTest");
+	}
+	
+	@Test
+	public void ModifyOrgTest() {
+		System.out.println("excute  ModifyOrgTest");
+	}
+	
+
+}
---
+++ src/test/java/pac1/ContactTest.java
@@ -0,0 +1,27 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void CreateContactTest() {
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser","chrome");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println("excute CreateContactTest");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		
+	}
+	
+	@Test
+	public void ModifyContactTest() {
+		System.out.println("excute ModifyContactTest");
+	}
+	
+
+}
